022fb232354d69449c24e770c7a116b4e7eaded7
Jason Darnell <jason.darnell@abaco.com>
2018-08-20 09:21:39 -0500

    Implement 'scan_directory'

---
+++ examples/scan_directory.py
@@ -0,0 +1,10 @@
+
+from opswat import MetaDefenderApi
+
+if __name__ == "__main__":
+
+    md = MetaDefenderApi(ip="10.26.50.15", port=8008)
+
+    results = md.scan_directory("files")
+
+    print(results)
--- opswat/meta_defender/meta_defender_api.py
+++ opswat/meta_defender/meta_defender_api.py
@@ -2,8 +2,12 @@
 import os
 from time import sleep
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 DEFAULT_PORT = 8008
+DELAY = 0.5
+NUM_THREAD_WORKERS = 25
+
 
 class MetaDefenderApi(object):
 
@@ -12,18 +16,18 @@ class MetaDefenderApi(object):
         self.port = port
 
     def scan_file(self, file_path):
-        data = self._get_file_data(file_path)
-        headers = {
-            "filename": os.path.basename(file_path)
-        }
-        post_url = f"http://{self.ip}:{self.port}/file"
-        r = requests.post(post_url, data=data, headers=headers)
-        data_id = r.json()['data_id']
-        sleep(2)
-        get_url = f"http://{self.ip}:{self.port}/file/{data_id}"
-        r = requests.get(get_url)
-        results = r.json()
-        return results
+        data_id = self._upload_file_for_processing(file_path)
+        return self._get_results(data_id)
+
+    def scan_directory(self, directory_path):
+        file_paths = self._get_file_paths_in_dir(directory_path)
+        executor = ThreadPoolExecutor(max_workers=NUM_THREAD_WORKERS)
+        futures = []
+        for file_path in file_paths:
+            futures.append(executor.submit(self.scan_file, file_path))
+        while any([(not future.done()) for future in futures]):
+            sleep(DELAY)
+        return [future.result() for future in futures]
 
     # -------------------------------------------------------------------------
     # Private
@@ -32,3 +36,30 @@ class MetaDefenderApi(object):
         with open(file_path, 'rb') as f:
             data = f.read()
         return data
+
+    def _upload_file_for_processing(self, file_path):
+        data = self._get_file_data(file_path)
+        headers = {
+            "filename": os.path.basename(file_path)
+        }
+        post_url = f"http://{self.ip}:{self.port}/file"
+        r = requests.post(post_url, data=data, headers=headers)
+        data_id = r.json()['data_id']
+        return data_id
+
+    def _get_results(self, data_id):
+        get_url = f"http://{self.ip}:{self.port}/file/{data_id}"
+        r = requests.get(get_url)
+        results = r.json()
+        while results['scan_results']['progress_percentage'] < 100:
+            sleep(DELAY)
+            r = requests.get(get_url)
+            results = r.json()
+        return results
+
+    def _get_file_paths_in_dir(self, directory_path):
+        file_paths = []
+        for subdir, dirs, files in os.walk(directory_path):
+            for file in files:
+                file_paths.append(os.path.join(subdir, file))
+        return file_paths
